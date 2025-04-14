f648d7d5ecc5d7109871d70bd06f5bad9b134408
aniketocto <aniketocto@gmail.com>
2025-04-14 16:44:47 +0530

    links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -56,7 +56,7 @@ const Footer = () => {
                     className="socialImg"
                   />
                 </a>
-                <a href="">
+                <a href="https://www.facebook.com/share/1DUG9Btxbg/" target="_blank">
                   <Image
                     src="/images/facebook.png"
                     alt="Facebook"
@@ -65,7 +65,7 @@ const Footer = () => {
                     className="socialImg"
                   />
                 </a>
-                <a href="">
+                <a href="https://www.linkedin.com/company/rooted-to-you/" target="_blank">
                   <Image
                     src="/images/linkedin.png"
                     alt="Linkedin"
