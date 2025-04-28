59fc809680a3d7514e27387d15eb5aff85ac33c5
Aniket Khambal <aniketocto@gmail.com>
2025-04-28 20:33:32 +0530

    img

--- app/(root)/b2b/page.jsx
+++ app/(root)/b2b/page.jsx
@@ -10,8 +10,8 @@ const Page = () => {
   const router = useRouter();
   return (
     <>
-      <section className="relative w-full h-dvh flex-col justify-center about-banner items-center mb-32 bg-cover bg-bottom flex">
-        <div className="flex flex-col items-start justify-center w-full md:w-[40%] gap-5 mt-[10%] py-5 px-8 bcb-banner">
+      <section className="relative w-full h-dvh flex-col justify-center about-banner items-start mb-32 bg-cover bg-bottom flex">
+        <div className="flex flex-col items-start justify-center w-full md:w-[40%] gap-5 mt-[10%] px-8 bcb-banner">
           <h1 className="secondary-font text-[#E6AF55] font-bold">
             Convenient.
             <br /> Diverse. <br />
