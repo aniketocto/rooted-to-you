fbac53adf15ba7fba78c17fa600a13735ed204af
Aniket Khambal <aniketocto@gmail.com>
2025-04-29 17:18:59 +0530

    changes

--- app/(root)/thank-you/page.jsx
+++ app/(root)/thank-you/page.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import './thanks.css'
+"use client";
+import React from "react";
+import { useSearchParams } from "next/navigation";
+import "./thanks.css";
 
 const page = () => {
-  return (
-    <section className='flex flex-col items-center justify-center h-[60vh] text-center'>
-         <img
-          src="/images/decorative.png"
-          className="absolute top-0 -z-1 h-[500px] md:w-full object-cover"
-          alt=""
-        />
-        <h1 className="thanks-head">Thank You</h1>
-        <p className='thanks-text'>Excited to serve you Rooted meals.</p>
-        
-    </section>
-  )
-}
+  const searchParams = useSearchParams();
+  const type = searchParams.get("type");
 
-export default page
+  let message = "Excited to serve you Rooted meals.";
+  if (type === "feedback-form-submission") {
+    message = "We value your feedback and try to improve day by day.";
+  } else if (type === "contact-form-submission") {
+    message = "Our executive will get in touch with you soon.";
+  }
+
+  return (
+    <section className="flex flex-col items-center justify-center h-[60vh] text-center relative">
+      <img
+        src="/images/decorative.png"
+        className="absolute top-0 -z-1 h-[500px] md:w-full object-cover"
+        alt=""
+      />
+      <h1 className="thanks-head">Thank You</h1>
+      <p className="thanks-text">{message}</p>
+    </section>
+  );
+};
+
+export default page;
